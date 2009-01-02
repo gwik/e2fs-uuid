e56a280fbb064184d9436beffc821fc845b9db2d
Antonin Amand <antonin.amand@feedback20.com>
2009-01-02 04:55:45 +0100

    fix linux linking

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,6 @@
 require 'mkmf'
 
+have_library('uuid')
 have_header('uuid/uuid.h') or fail('missing uuid/uuid.h header')
 have_type('uuid_t', 'uuid/uuid.h') or fail('missing type uuid_t')
 create_makefile('uuid_ext')
